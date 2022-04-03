42702f6cd3a59a85de14fdaba0958348728f4f8a
mr cod3r <mrcod3r.ir@gmail.com>
2022-04-03 11:49:04 +0430

    p28 completed add vueuse

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
-<script></script>
+<script>
+	import { useLocalStorage, useMouse, usePreferredDark } from "@vueuse/core"
+
+	export default {
+		setup() {
+			// tracks mouse position
+			const { x, y } = useMouse()
+
+			// is user prefers dark theme
+			const isDark = usePreferredDark()
+
+			// persist state in localStorage
+			const store = useLocalStorage("my-storage", {
+				name: "Apple",
+				color: "red"
+			})
+
+			return { x, y, isDark, store }
+		}
+	}
+</script>
 <template>
 	<div>
 		<h1>page</h1>
+		<h2>x : {{ x }}</h2>
+		<h2>y : {{ y }}</h2>
+		<h2>store: {{ store }}</h2>
 	</div>
 </template>
