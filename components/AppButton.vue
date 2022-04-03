d915823447442252b83a97cb39927dff4fd071cc
mr cod3r <mrcod3r.ir@gmail.com>
2022-04-03 12:43:08 +0430

    p31 completed

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -5,9 +5,18 @@
 </template>
 
 <script setup lang="ts">
-	const props = defineProps({
-		variant: { type: String, default: "primary" },
-		loading: { type: Boolean, default: false }
+	// const props = defineProps({
+	// 	variant: { type: String, default: "primary" },
+	// 	loading: { type: Boolean, default: false }
+	// })
+
+	interface Props {
+		variant?: string
+		loading?: boolean
+	}
+	const props = withDefaults(defineProps<Props>(), {
+		variant: "primary",
+		loading: false
 	})
 </script>
 
